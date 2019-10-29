19d815dac82acf9a998f40f5814336787be48143
Dario Campagna <campagna@esteco.com>
2019-10-29 16:58:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python TDD template
+
+Template project for practicing TDD with Python `unittest` and Visual Studio Code.
---
+++ test/test_this.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class MyTest(unittest.TestCase):
+    def test_will_fail(self):
+        self.assertTrue(False)

9c1dd93d7ea639ea9cb8314d2e8e795f232af052
Dario Campagna <campagna@esteco.com>
2019-10-30 09:36:05 +0100

    Extract function
    Signed-off-by: Dario Campagna <campagna@esteco.com>

--- production/string_calculator.py
+++ production/string_calculator.py
@@ -1,14 +1,22 @@
 import re
 
+
 def add(numbers):
     if '' == numbers:
         return 0
     else:
         return sum([int(x) for x in __tokenize__(numbers)])
 
+
 def __tokenize__(numbers):
-    regex = ',|\n'
-    if (numbers.startswith('//')):
-        regex = numbers[2]
-        numbers = numbers[4:]
-    return re.split(regex, numbers)
+    delimiter_regex, numbers = __split_into_delimiter_and_numbers__(numbers)
+    return re.split(delimiter_regex, numbers)
+
+
+def __split_into_delimiter_and_numbers__(numbers):
+    pattern = re.compile(r"//(.)\n(.+)")
+    match = pattern.match(numbers)
+    if match:
+        return (match.group(1), match.group(2))
+    else:
+        return (',|\n', numbers)
